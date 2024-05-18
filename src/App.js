ce0b82fd48914a3c8bc9c122b756abdda4aca49d
Mdfahimiftekhar <iftekharfahim3163@gmail.com>
2024-05-17 19:13:46 -0500

    Browser router changed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // import Home from "./components/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import SearchPage from "./components/SearchPage";
 import ResultPage from "./components/ResultPage";
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename="/">
       <Routes>
         <Route path="/" element={<SearchPage />} />
         <Route path="/result/:username" element={<ResultPage />} />
         <Route path="*" element={<SearchPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./css/Style.css";
 import App from "./App";
 
